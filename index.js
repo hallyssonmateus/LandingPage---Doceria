d5715e682d30e6e5079e51043af38a7f19382656
hallyssonmateus <hallysson_gato@hotmail.com>
2024-08-03 16:47:51 -0300

    Atualizando header, como logo e menu responsivo ao click usuario

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    const navContain = document.querySelector('.nav-contain');
+    const button = document.getElementById('menu-button');
+    
+    if (navContain.style.display === 'flex') {
+        navContain.style.display = 'none';
+        button.textContent = 'v'; // Mude o texto do botão de volta
+    } else {
+        navContain.style.display = 'flex';
+        button.textContent = '^'; // Mude o texto do botão para indicar que pode ser fechado
+    }
+});
